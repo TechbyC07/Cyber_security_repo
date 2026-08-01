0713fc988ee975a07d6e767b9cb5431127f59090
Caleb MN Mukonka <calebmukonka@gmail.com>
2026-08-01 23:02:40 +0200

    Add files via upload

---
+++ Password_manager(Hashing).py
@@ -0,0 +1,38 @@
+import hashlib
+import getpass
+
+#store the key value  pairs
+password_manager = {}
+
+def create_account():
+    username = input("Enter your username; ")
+    password = getpass.getpass("Enter your password: ")
+    hashed_password = hashlib.sha256(password.encode()).hexdigest()
+    password_manager[username] = hashed_password
+    print("Account created successfully!")
+
+def login():
+    username = input("Enter your username; ")
+    password = getpass.getpass("Enter your password: ")
+    hashed_password = hashlib.sha256(password.encode()).hexdigest()
+    if username in password_manager.keys() and password_manager[username] == hashed_password:
+        print("Login successful!")
+    else:
+        print("Invalid username or password.")
+
+def main():
+    while True:
+        choice = input("Enter 1. To create an account,\
+                       Enter 2. To Login to an existing account\
+                       Enter 3. TO exit this menu")
+        if choice == "1":
+            create_account()
+        elif choice == "2":
+            login()
+        elif choice == "0":
+            break
+        else:
+            print("Invalid choice.")
+
+if __name__ == "__main__":
+    main()
